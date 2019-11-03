f1c2e0c88e75a3b4e26c6aa12c7569853c77a5f0
Bill Hammond <willmhammond@gmail.com>
2019-11-02 23:41:41 -0700

    initial commit; crerated static shell

--- src/App.js
+++ src/App.js
@@ -1,26 +1,83 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import './style.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+
+  state = {
+    clickedCharacters: [],
+    score= 0,
+    characters: [
+      {
+        img: require("./charImages/fox.jpeg"),
+        id: 1
+      },
+      {
+        img: require("./charImages/AshFox.gif"),
+        id: 2
+      },
+      {
+        img: require("./charImages/badger.jpg"),
+        id: 3
+      },
+      {
+        img: require("./charImages/bean.jpeg"),
+        id: 4
+      },
+      {
+        img: require("./charImages/Boggis.jpg"),
+        id: 5
+      },
+      {
+        img: require("./charImages/Bunce.jpg"),
+        id: 6
+      },
+      {
+        img: require("./charImages/kris.jpg"),
+        id: 7
+      },
+      {
+        img: require("./charImages/Kylie.jpg"),
+        id: 8
+      },
+      {
+        img: require("./charImages/Mrs_Fox.jpg"),
+        id: 9
+      },
+    ]
+  }
+
+  onClick = (id) => {
+    let { clickedCharacters } = this.state;
+    if (clickedCharacters.includes(id)) {
+      console.log('you lost')
+      clickedCharacters = []
+      score = 0
+    } else {
+      score++
+      clickedCharacters.push(id)
+    }
+
+    clickedCharacters.push(id)
+    this.setState({
+      clickedCharacters,
+      score
+    })
+  }
+
+  render() {
+
+  
+    return (
+      <div className="App">
+        <h1>Clicky</h1>
+        <p>Click on each picture once without repeating to win</p>
+        <div className="img-zone">
+        {this.state.characters.map(character => <img onClick= {this.onClick} key={character.id} src={character.img} alt="placeholder" /> 
+        )}
+        </div>
+      </div>
+    );
+  } 
 }
 
 export default App;
