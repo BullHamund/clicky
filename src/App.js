d53c216c161ba9c845d2841096c0f440faf50fd8
Bill Hammond <willmhammond@gmail.com>
2019-11-05 17:05:15 -0800

    finished static version

--- src/App.js
+++ src/App.js
@@ -5,49 +5,50 @@ class App extends Component {
 
   state = {
     clickedCharacters: [],
-    score= 0,
+    score: 0,
+    roundEnd: false,
     characters: [
       {
-        img: require("./charImages/fox.jpeg"),
+        img: require("./charImages/fox.png"),
         id: 1
       },
       {
-        img: require("./charImages/AshFox.gif"),
+        img: require("./charImages/AshFox.png"),
         id: 2
       },
       {
-        img: require("./charImages/badger.jpg"),
+        img: require("./charImages/badger.png"),
         id: 3
       },
       {
-        img: require("./charImages/bean.jpeg"),
+        img: require("./charImages/bean.png"),
         id: 4
       },
       {
-        img: require("./charImages/Boggis.jpg"),
+        img: require("./charImages/Boggis.png"),
         id: 5
       },
       {
-        img: require("./charImages/Bunce.jpg"),
+        img: require("./charImages/Bunce.png"),
         id: 6
       },
       {
-        img: require("./charImages/kris.jpg"),
+        img: require("./charImages/kris.png"),
         id: 7
       },
       {
-        img: require("./charImages/Kylie.jpg"),
+        img: require("./charImages/Kylie.png"),
         id: 8
       },
       {
-        img: require("./charImages/Mrs_Fox.jpg"),
+        img: require("./charImages/Mrs_Fox.png"),
         id: 9
-      },
+      }
     ]
   }
 
   onClick = (id) => {
-    let { clickedCharacters } = this.state;
+    let { clickedCharacters, score, characters } = this.state;
     if (clickedCharacters.includes(id)) {
       console.log('you lost')
       clickedCharacters = []
@@ -56,11 +57,23 @@ class App extends Component {
       score++
       clickedCharacters.push(id)
     }
+    let characterCount = characters.length;
+      let characterSelected;
+      let temp;
 
-    clickedCharacters.push(id)
+      while (characterCount > 0) {
+        characterSelected = Math.floor(Math.random() * characterCount);
+        characterCount--;
+        temp = characters[characterCount];
+        characters[characterCount] = characters[characterSelected];
+        characters[characterSelected] = temp;
+      }
+
+    // clickedCharacters.push(id)
     this.setState({
       clickedCharacters,
-      score
+      score,
+      characters
     })
   }
 
@@ -72,7 +85,7 @@ class App extends Component {
         <h1>Clicky</h1>
         <p>Click on each picture once without repeating to win</p>
         <div className="img-zone">
-        {this.state.characters.map(character => <img onClick= {this.onClick} key={character.id} src={character.img} alt="placeholder" /> 
+        { this.state.characters.map(character => <img onClick= {() =>  this.onClick (character.id)} key={character.id} src={character.img} alt="placeholder" /> 
         )}
         </div>
       </div>
